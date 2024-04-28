dd1b76bc91095900a632fc519a4decb78ce4ffe4
Thomas Holder <thomas@thomas-holder.de>
2024-04-28 08:34:17 +0200

    Reverse Proxy Support

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 from functools import update_wrapper, wraps
 from flask import Flask, make_response, request, render_template
+from flask import Blueprint, render_template
 
 from . import database
-
-app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 def jsonifyexceptions(func):
@@ -37,12 +35,16 @@ def nocache(view):
     return update_wrapper(no_cache, view)
 
 
-@app.route("/")
+blueprint = Blueprint('the_blueprint', __name__, template_folder='templates')
+
+
+@blueprint.route("/")
 @nocache
 def home():
     return render_template("index.html")
 
-@app.route("/statusmask", methods=["GET"])
+
+@blueprint.route("/statusmask", methods=["GET"])
 @nocache
 def statusmask_get():
     return {
@@ -50,7 +52,8 @@ def statusmask_get():
         "data": database.get_status_mask(),
     }
 
-@app.route("/statusmask", methods=["POST"])
+
+@blueprint.route("/statusmask", methods=["POST"])
 @jsonifyexceptions
 def statusmask_post():
     mask = request.get_json()["data"]
@@ -58,9 +61,18 @@ def statusmask_post():
     return {"status": "success"}
 
 
-@app.route("/reserve/<int:desk_id>")
+@blueprint.route("/reserve/<int:desk_id>")
 @nocache
 @jsonifyexceptions
 def reserve(desk_id: int):
     database.make_reservation(desk_id)
     return {"status": "success"}
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.register_blueprint(blueprint)
+
+# For reverse proxy with HTTPS... not sure if needed
+# app.register_blueprint(blueprint, url_prefix="/zeiss-hackathon-2024", name="for_https_proxy")
